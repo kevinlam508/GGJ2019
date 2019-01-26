a2d6a1a8d3993e24784c29ecc4a8ab408dc079c4
kevin <kevin.lam72003@gmail.com>
2019-01-26 10:57:36 -0600

    folders

---
+++ AppartmentGame/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Exodrifter.Rumor.Engine;
+
+public class Dialogue : MonoBehaviour
+{
+
+
+	private Rumor rumor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
