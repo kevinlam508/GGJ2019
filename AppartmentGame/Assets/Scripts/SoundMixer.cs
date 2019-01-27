709bc1c42ebca23aa5e733d664fe867afe1e3205
kevin <kevin.lam72003@gmail.com>
2019-01-27 03:05:46 -0600

    cleaned UI in dialogue test

--- AppartmentGame/Assets/Scripts/SoundMixer.cs
+++ AppartmentGame/Assets/Scripts/SoundMixer.cs
@@ -20,7 +20,6 @@ public class SoundMixer : MonoBehaviour
 	[SerializeField] float fadeSpeed = .1f;
 	[SerializeField] AudioClip[] music;
 
-	private int activeTracks;
 	private int fadeInTracks;
 	private int fadeOutTracks;
 	private AudioSource[] tracks;
@@ -29,15 +28,65 @@ public class SoundMixer : MonoBehaviour
     void Start()
     {
         tracks = new AudioSource[music.Length];
+        for(int i = 0; i < music.Length; ++i){
+        	tracks[i] = gameObject.AddComponent<AudioSource>();
+        	tracks[i].clip = music[i];
+        	tracks[i].Play();
+        	tracks[i].loop = true;
+        	tracks[i].volume = 0;
+        }
+
+        MakeFadeIn(AudioTracks.HOME_BASE);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < tracks.Length; ++i){
+        	int flag = 1 << i;
+        	bool fadeIn = (flag & fadeInTracks) != 0;
+        	bool fadeOut = (flag & fadeOutTracks) != 0;
+        	Debug.Assert(!(fadeIn && fadeOut));
+
+        	if(fadeIn){
+        		Fade(tracks[i], fadeSpeed, 0, 1);
+        		if(tracks[i].volume == 1){
+        			fadeInTracks = fadeInTracks & (~flag);
+        		}
+        	}
+        	if(fadeOut){
+        		Fade(tracks[i], fadeSpeed, 1, 0);
+        		if(tracks[i].volume == 0){
+        			fadeOutTracks = fadeOutTracks & (~flag);
+        		}
+        	}
+        }
     }
 
     void Fade(AudioSource track, float speed, float start, float end){
     	track.volume = ((end - start) * speed * Time.deltaTime) + track.volume;
+    }
+
+    public void MakeFadeIn(AudioTracks track){
+    	int flag = 1 << (int)track;
+    	fadeInTracks |= flag;
+    	fadeOutTracks &= (~flag);
+    }
+
+    public void MakeFadeOut(AudioTracks track){
+    	int flag = 1 << (int)track;
+    	fadeOutTracks |= flag;
+    	fadeInTracks &= (~flag);
+    }
+
+    public void MakeFadeIn(int track){
+    	int flag = 1 << (int)track;
+    	fadeInTracks |= flag;
+    	fadeOutTracks &= (~flag);
+    }
+    public void MakeFadeOut(int track){
+    	int flag = 1 << (int)track;
+    	fadeOutTracks |= flag;
+    	fadeInTracks &= (~flag);
     }
 }
