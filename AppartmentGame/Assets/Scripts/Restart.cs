9c68f589f3072f6d00f2910e85ec7202a2e3223f
TINH3AD <tinh3ad@gmail.com>
2019-02-16 02:46:42 -0600

    atoned for some sins

---
+++ AppartmentGame/Assets/Scripts/Restart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){
+        	GameState.state = State.DIALOGUE;
+        	GameState.res = MinigameResult.LOSE;
+        	SceneManager.LoadScene(0);
+        }
+        if(Input.GetKeyDown(KeyCode.Escape)){
+        	Application.Quit();
+        }
+    }
+}
